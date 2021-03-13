2f6b0565f2f899b0e031b6b53f199fce68170855
eneylton <eneylton@hotmail.com>
2021-03-13 10:19:00 -0300

    logi-admin

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+
+
+include __DIR__.'/includes/login/header.php';
+include __DIR__.'/includes/login/content.php';
+include __DIR__.'/includes/login/footer.php';
+
+
+?>
